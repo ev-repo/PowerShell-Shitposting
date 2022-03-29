a59a686a4d314e91d8cfbb2bb269170967eb8867
Evan Berkowitz <102604146+ev-repo@users.noreply.github.com>
2022-03-29 15:28:34 -0500

    CSV-UserCreate.ps1 Version 1.1
    Changelog
    - Changed version number from 1.0 to 1.1

--- CSV-UserCreate.ps1
+++ CSV-UserCreate.ps1
@@ -1,6 +1,6 @@
 <#
 Name: CSV-UserCreate.ps1
-Version 1.0
+Version 1.1
 Description: MIGHT create new users from .csv (probably not), no clue don't have AD environment to test it rn 
 Author: Evan Berkowitz
 Date: 3/29/2022
@@ -24,7 +24,7 @@ foreach ($User in $ADUsers) {
     $userPW = [System.Web.Security.Membership]::GeneratePassword(10, 3)
 
     #Creates AD user using info given
-    New-ADUser -GivenName $firstName -Surname $lastName -UserPrincipalName $username -EmailAddress $email -OfficePhone $phone -Path $OU -AccountPassword (ConvertTo-SecureString $userPW -AsPlainText -Force)
+    New-ADUser -GivenName $firstName -Surname $lastName -SamAccountName $username -EmailAddress $email -OfficePhone $phone -Path $OU -AccountPassword (ConvertTo-SecureString $userPW -AsPlainText -Force)
     Write-Host "Created user $username 'nPassword: $userPW"
 
     #BOMB ASS GROUP ADDER
